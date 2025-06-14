5193f30e3b89056add89d21c2d84098c68440352
Iviwe <booiiviwe4394@gmail.com>
2025-06-14 23:45:17 +0200

    new contacts page

--- buttons.js
+++ buttons.js
@@ -2,3 +2,10 @@
 function navigate_to_contacts(){
     window.open("contactsPage.html", "_self");
 }
+
+
+// function to navigate to the home page
+function navigate_to_home(){
+    window.open("index.html", "_self");
+}
+
--- functions.js
+++ functions.js
@@ -9,6 +9,7 @@ function fetchContacts(){
         })
 }
 
+
 function displayOutput(data){
     output ="<table>";
     for(a in data){
@@ -31,3 +32,46 @@ function displayOutput(data){
     output += "</table>"
     document.getElementById("table").innerHTML = output;
 }
+
+
+
+function fetchAllContacts(){
+        fetch(rootPath + "controller/get-contacts/")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+          displayAllOutput(data);
+        })
+}
+
+
+function displayAllOutput(data){
+    new_output ="<table>";
+    for(a in data){
+        new_output += `
+            <tr>
+                <td>
+                    <div id="contact-list-box">
+                        <div class="contact-list">
+                            <div class="contact-profile-pic" style="background-image: url(${rootPath}controller/uploads/${data[a].avatar});background-size: cover;background-position: center;background-repeat: no-repeat;"></div>
+                            <div class="contact-profile-details">
+                                <h2 class="contact-name">${data[a].firstname} ${data[a].lastname}</h2>
+                                <p class="contact-number">${data[a].mobile}</p>
+                            </div>
+                            <div id="action-icons">
+                                <i id="star" class="fa-solid fa-star"></i>
+                                <i id="bin" class="fa-solid fa-trash"></i>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        `
+    }
+    new_output += "</table>"
+    document.getElementById("new_table").innerHTML = new_output;
+}
+
+
+
