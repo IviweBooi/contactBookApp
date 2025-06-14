636a544fa66560b01e928bd9f98afc2a26a9f269
Iviwe <booiiviwe4394@gmail.com>
2025-06-14 18:04:59 +0200

    added lil animation

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ let apiKey = checkApiKey();
 // Function to verify the presence of an API key
 function checkApiKey() {
     // If there is no API key stored, redirect the user to the API key entry page
-    // and load in the same frame as the current page
+    // and load in the same window as the current page
     if (!localStorage.getItem("apiKey")) {
         window.open("enter-api-key.html", "_self"); // Open the API key entry page
     }
---
+++ functions.js
@@ -0,0 +1,27 @@
+
+function fetchContacts(){
+        fetch(rootPath + "controller/get-contacts/")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+          displayOutput(data);
+        })
+}
+
+function displayOutput(data){
+    output = "<table>";
+
+    for(a in data){
+        output += `
+            <tr>
+                <td><img src="${rootPath}controller/uploads/${data[a].avatar}" width='40'/></td>
+                <td><h5>${data[a].firstname}</h5></td>
+                <td><h5>${data[a].lastname}</h5></td>
+                <td><h5>${data[a].mobile}</h5></td>
+            </tr>
+        `
+    }
+    output += "</table>";
+    document.getElementById("table").innerHTML = output;
+}
